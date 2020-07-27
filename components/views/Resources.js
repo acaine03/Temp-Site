dfdd5d7a0fe579a1a54317dc41286822bd252118
Arthur Caine III <acaine03@gmail.com>
2020-07-27 13:43:43 -0500

    Added links to resources, updated contact us page.

--- components/views/Resources.js
+++ components/views/Resources.js
@@ -9,25 +9,25 @@ export default () => `
             <div class="affiliates">
                 
                 <div class="group-1">
-                <a href="">SAMHSA</a><figcaption>Substance Abuse & Mental Health Services</figcaption><br>
-                <a href="">MCADSV</a><figcaption>Sexual & Domestic Violence</figcaption><br>
-                <a href="">DMH</a><figcaption>Dept. of Mental Health</figcaption><br>
+                <a href="https://www.samhsa.gov/about-us" target="_blank">SAMHSA</a><figcaption>Substance Abuse & Mental Health Services</figcaption><br>
+                <a href="https://www.mocadsv.org/" target="_blank">MCADSV</a><figcaption>Sexual & Domestic Violence</figcaption><br>
+                <a href="https://dmh.mo.gov/programs" target="_blank">DMH</a><figcaption>Dept. of Mental Health</figcaption><br>
                 </div>
 
                 <div class="group-2">
-                <a href="">ALIVE</a><figcaption>Intimate Partner Violence</figcaption><br>
-                <a href="">Imani Therapy</a><figcaption>North County Counseling</figcaption><br>
+                <a href="https://alivestl.org/about-alive/" target="_blank">ALIVE</a><figcaption>Intimate Partner Violence</figcaption><br>
+                <a href="http://niagroupstl.squarespace.com/imani-therapy-home/" target="_blank">Imani Therapy</a><figcaption>North County Counseling</figcaption><br>
                 </div>
 
                 <div class="group-3">
-                <a href="">A&A Inspirations</a><figcaption>St. Louis City Counseling</figcaption><br>
-                <a href="">MMHCA</a><figcaption>Mental Health Services</figcaption><br>
-                <a href="">Kathy J. Weinman</a><figcaption>Domestic Violence Shelter</figcaption><br>
+                <a href="https://www.aainspirations.com/" target="_blank">A&A Inspirations</a><figcaption>St. Louis City Counseling</figcaption><br>
+                <a href="http://www.mmhca.com/index.html" target="_blank">MMHCA</a><figcaption>Mental Health Services</figcaption><br>
+                <a href="https://www.stlouisco.com/Health-and-Wellness/Human-Services/Domestic-Violence-Program" target="_blank">Kathy J. Weinman</a><figcaption>Domestic Violence Shelter</figcaption><br>
                 </div>
 
                 <div class="group-4">
-                <a href="">KHAOS Inc.</a><figcaption>Group Counseling</figcaption><br>
-                <a href="">ACA</a><figcaption>American Counseling Assoc.</figcaption><br>
+                <a href="http://www.khaosinc.org/index.html" target="_blank">KHAOS Inc.</a><figcaption>Group Counseling</figcaption><br>
+                <a href="https://www.counseling.org/about-us/about-aca" target="_blank">ACA</a><figcaption>American Counseling Assoc.</figcaption><br>
                 </div>
 
             </div>
